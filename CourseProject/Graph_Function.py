ec11cd6e22886589b4a6e2155f1620820003ad98
JamesNorthup <JamesNorthup@users.noreply.github.com>
2022-08-16 19:59:02 +0000

    csv and pandas

--- CourseProject/Graph_Function.py
+++ CourseProject/Graph_Function.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+### try except
 import sympy as sym
 from sympy import *
 import numpy as np
@@ -10,54 +10,57 @@ import matplotlib.pyplot as plt
 from sympy import lambdify
 import numpy as np
 from IPython.display import display
+import yaml
+import climage
 
+def getDisplayText():
+    with open("AppResponseLabels.yaml","r") as pod:
+        displayTxt = yaml.load(pod, Loader=yaml.FullLoader)
+    return displayTxt
 
-def PlotXY_DATA(FN):
-    x = sym.Symbol("x")
-    x_range = input("Give range of x: Example -20, 20").split(",")
-    x_min = int(x_range[0])
-    x_max = int(x_range[1])
-
-    x_List = np.array(list(range(x_min, x_max)))
-
-    FN_R_Func = lambdify(x, FN, "numpy")
-    y_List = FN_R_Func(x_List)
-
-    df = pd.DataFrame({"x": x_List, "y": y_List})
-    df.to_csv("x_y_Function_Data", sep="\t")
-    df.plot("x", "y", kind="line")
-    plt.title("Function Graph")
-    plt.savefig("Function_Graph.png")
-    return df
-
-
-def main():
-
-    x = sym.Symbol("x")
-    y = sym.Symbol("y")
-    print(
-        "Methods:",
-        "Integration: integrate()",
-        "Differentiate: diff()",
-        "Limit: limit",
-        "SolveForRoot: root",
-        sep="\n",
-        end="\n\n",
-    )
-    print(
-        "Variables include:\tx,y \nConstants include \nEulers e:\tE\nComplex i:\tI",
-        flush=True,
-    )
-
-    Cont = True
-
-    print("Type q to quit")
-    inputFunction = input("Enter Function to be graphed")
-
-    FN = sym.sympify(inputFunction)
+def Help():
+    
+    print(displayTxt["HELP_TEXT"])
+    
+def PlotXY_DATA(userInput, Range_x_y):
+#     userInput = input("Enter Function:  ")
+    x=sym.Symbol('x')
+    FN = sym.sympify(userInput)
+    
+    
     display(FN)
-    PlotXY_DATA(FN)
 
+#     Range_x_y=list(map(int,input(displayTxt["RANGE_TEXT"]).strip().split()))[:2]
+   
+    x_List=np.array(list(range(Range_x_y[0],Range_x_y[1])))
+    
+    FN_R_Func= lambdify(x,FN,'numpy')
+    y_List=FN_R_Func(x_List)
+    
+    df = pd.DataFrame({'x':x_List, 'y':y_List})
+    df.to_csv("x_y_Function_Data", sep='\t')
+    df.plot('x', 'y')
+    plt.savefig('GRAPH.png') 
+    print(climage.convert('GRAPH.png'))
+def main():
+    displayTxt=getDisplayText()
 
+    cont = True
+
+    while cont:
+        x=sym.Symbol('x')
+        
+        print(displayTxt["CHOOSE_TEXT"])
+        userInput = input().lower()
+        
+        if(userInput=="h"):
+            print(displayTxt["HELP_TEXT"])
+        elif(userInput=="q"):
+            cont=False
+        elif (userInput=="s"):
+            userInput = input("Enter Function:  ")
+            Range_x_y=list(map(int,input(displayTxt["RANGE_TEXT"]).strip().split()))[:2]
+            PlotXY_DATA(userInput, Range_x_y)
+        
 if __name__ == "__main__":
     main()
